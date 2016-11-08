dc1ab5f0595b639e2094dd79b95591b1c2ca9961
oomori-you <valsamina@gmail.com>
2016-11-08 23:31:50 +0900

    日本語再生を試す

--- hello.py
+++ hello.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 from slackclient import SlackClient
-from flask import Flask
+from flask import request, Flask
 import twilio.twiml
 import os
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+  from_number = request.values.get('From', None)
+  
   token = os.environ["SLACK_API_TOKEN"]
+  text = ""
+  if from_number is None:
+    text = "Hello anonymous :tada:"
+  else:
+    # Greet the caller by name
+    text = "Hello {0} :tada:".format(from_number)
   sc = SlackClient(token)
   sc.api_call(
     "chat.postMessage",
     channel="#random",
-    text="Hello from Python! :tada:"
+    text=text
   )
 
   resp = twilio.twiml.Response()
-  resp.say(u"日本語にほんご")
+  resp.say(u"日本語にほんご", language="ja-jp")
 
   return str(resp)
 
