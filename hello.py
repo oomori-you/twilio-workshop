69a090f6ec569b2acfd8e654299c6f4cebb94cd1
oomori-you <valsamina@gmail.com>
2016-11-08 21:25:49 +0900

    呼び出し時にslackに痕跡を残すようにした

--- hello.py
+++ hello.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+from slackclient import SlackClient
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+  token = os.environ["SLACK_API_KEY"]
+  sc = SlackClient(token)
+  sc.api_call(
+    "chat.postMessage",
+    channel="#random",
+    text="Hello from Python! :tada:"
+  )
+  return 'Hello World!!!'
+
+
 
 if __name__ == '__main__':
     app.run()
